9ef43c9c0f4885a5f49938d5ebfca97969021fea
Nursultan1311 <200103440@stu.sdu.edu.kz>
2023-02-10 23:52:06 +0600

    sql connected

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import ("fmt"
         "net/http"
         "database/sql"
-        
+
       	_ "github.com/go-sql-driver/mysql")
 
 
@@ -18,5 +18,10 @@ func handleRequest(){
 }
 func main()  {
   handleRequest()
-
+     db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/golang")
+     if err != nil {
+       panic(err)
+     }
+     defer db.Close()
+     fmt.Println("Connection with MySQL")
 }
