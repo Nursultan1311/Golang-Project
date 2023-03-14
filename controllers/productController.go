a43f060a5bd75653cd4cbd34f0b12a87f0ebc450
Nursultan1311 <92705319+Nursultan1311@users.noreply.github.com>
2023-03-14 20:15:56 +0600

    Update productController.go

--- controllers/productController.go
+++ controllers/productController.go
@@ -11,40 +11,36 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func ShowProducts(res http.ResponseWriter, req *http.Request) {
+func ShowProducts(res http.ResponseWriter, req *http.Request) { // we get all products from database with by GetProductsfubction
 	tpl, _ = tpl.ParseGlob("templates/*.html")
 	tpl.ExecuteTemplate(res, "showProducts.html", GetProducts())
 	return
 }
 
-func GetProducts() []models.Product {
-	db = database.ConnectToDB()
+func GetProducts() []models.Product { // Getting all products from database
+	db = database.ConnectToDB() // connecting to database
 
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	rows, err := db.Query("SELECT name, description, price, quantity FROM products")
+	rows, err := db.Query("SELECT name, description, price, quantity FROM products") // selecting all products
 	defer rows.Close()
-	var products []models.Product
+	var products []models.Product // declaration of slice 
 	for rows.Next() {
 		var product models.Product
 		if err := rows.Scan(&product.Name, &product.Description, &product.Price, &product.Quantity); err != nil {
 			return nil
 		}
-		products = append(products, product)
+		products = append(products, product) // adding each products for products slice
 	}
 
 	if err != nil {
 		log.Fatal(err)
 	}
-	return products
+	return products // then return products for showProducts function
 }
 
-func GetProduct(res http.ResponseWriter, req *http.Request) {
-	db = database.ConnectToDB()
+func GetProduct(res http.ResponseWriter, req *http.Request) { // Getting all product with by search input from header
+	db = database.ConnectToDB() // connecting to database
 
-	name := req.FormValue("Target")
+	name := req.FormValue("Target") // getting target value from request post with by name Target
 
 	fmt.Println(name)
 
@@ -52,7 +48,7 @@ func GetProduct(res http.ResponseWriter, req *http.Request) {
 		log.Fatal(err)
 	}
 
-	rows, err := db.Query("SELECT name, description, price, quantity FROM products")
+	rows, err := db.Query("SELECT name, description, price, quantity FROM products") // selecting all products to slice
 	defer rows.Close()
 	var products []models.Product
 	for rows.Next() {
@@ -65,8 +61,9 @@ func GetProduct(res http.ResponseWriter, req *http.Request) {
 
 	var result []models.Product
 	for _, i := range products {
+		// comparing product name with target value from search
 		if strings.Contains(strings.ToLower(i.Name), strings.ToLower(name)) || strings.ToLower(i.Name) == strings.ToLower(name) {
-			result = append(result, i)
+			result = append(result, i) // if products exist with target name, then we add it to result slice
 		}
 	}
 
@@ -74,26 +71,26 @@ func GetProduct(res http.ResponseWriter, req *http.Request) {
 		log.Fatal(err)
 	}
 	fmt.Println(result)
-	tpl.ExecuteTemplate(res, "showProducts.html", result)
+	tpl.ExecuteTemplate(res, "showProducts.html", result) // then return all products with target value name
 }
 
-func AddProduct(res http.ResponseWriter, req *http.Request) {
-	db = database.ConnectToDB()
+func AddProduct(res http.ResponseWriter, req *http.Request) { // adding a new product from website to database
+	db = database.ConnectToDB() // connecting
 
 	if req.Method != "POST" {
 		http.ServeFile(res, req, "templates/add_product.html")
 		return
 	}
-
+	// getting data from form
 	description := req.FormValue("description")
 	price := req.FormValue("price")
 	quantity := req.FormValue("quantity")
 	name := req.FormValue("name")
-
+	// inserting a new data to database
 	_, err = db.Exec("INSERT INTO products(name, description, price, quantity) VALUES(?, ?, ?, ?)", name, description, price, quantity)
 	if err != nil {
 		http.Error(res, "Server error, unable to create your account.", 500)
 		return
 	}
-	http.Redirect(res, req, "/show_products", 301)
+	http.Redirect(res, req, "/show_products", 301) // redirecting the page
 }
