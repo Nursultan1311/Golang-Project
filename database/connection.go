77b8f0facc92168ead1f80bf8ca3b985ac25b5b1
Nursultan1311 <92705319+Nursultan1311@users.noreply.github.com>
2023-03-14 19:14:38 +0600

    Update connection.go

--- database/connection.go
+++ database/connection.go
@@ -8,6 +8,8 @@ import (
 var db *sql.DB
 var err error
 
+// In this page we connect go project with database. For this we enter name of sql user, his password then database name which you want to connect 
+
 func ConnectToDB() *sql.DB {
 	// Connecting to database.  "username:password@(127.0.0.1:8889)/databasename"
 	db, err = sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/go") 
